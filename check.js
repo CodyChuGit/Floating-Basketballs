3f763c7b5bbba50a38d2109cb74be799b67b81d8
Cody <contact@codychu.com>
2026-02-28 18:57:23 +0800

    Clean up project: remove temporary scripts, large test files, and unused components

--- check.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-const data = fs.readFileSync('/Users/cody/react-3d-model/public/Ball.gltf', 'utf8');
-const gltf = JSON.parse(data);
-console.log("Nodes:", gltf.nodes);
-console.log("Meshes:", gltf.meshes);
--- check_mat.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-const data = fs.readFileSync('/Users/cody/react-3d-model/public/Ball.gltf', 'utf8');
-const gltf = JSON.parse(data);
-console.log("Materials:", gltf.materials.map(m => m.name));
-console.log("PBR:", gltf.materials[0].pbrMetallicRoughness);
